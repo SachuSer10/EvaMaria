df7c676e4412e091a76ed1cb2a55b14b006b06de
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2022-08-21 08:05:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,7 +16,7 @@ class script(object):
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    MANUELFILTER_TXT = """Help: <b>Filters</b>"""
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
